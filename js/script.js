e981f0581514b1e10c2f07ca4b105ede52f32d38
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-22 20:01:09 +0100

    [Update] Added comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        console.log("Fizz");
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else if (i % 15 == 0) {
-        console.log("FizzBuzz")
-    } else {
-        console.log(i)
+    if (i % 3 == 0) {                   // Se i è divisibile per 3
+        console.log("Fizz");            // Stampa Fizz
+    } else if (i % 5 == 0) {            // Se i è divisibile per 5
+        console.log("Buzz");            // Stampa Buzz
+    } else if (i % 15 == 0) {           // Se i è divisibile per 15
+        console.log("FizzBuzz");        // Stampa FizzBuzz
+    } else {                            // In tutti gli altri casi
+        console.log(i)                  // Stampa il valore di I
     }
 }
