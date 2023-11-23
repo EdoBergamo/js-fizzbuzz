ce3b1493b986db9b0784b2772873c40500c4ad81
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-23 17:12:59 +0100

    [UPDATE] Added bootrap and container div wrapper

--- js/script.js
+++ js/script.js
@@ -4,5 +4,5 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) output += "Fizz";
     if (i % 5 === 0) output += "Buzz";
     console.log(output || i);
-    list.innerHTML += `<li class="list-group-item">${output || i}</li>`;
+    list.innerHTML += `<li class="list-group-item list-group-item-action">${output || i}</li>`;
 }
