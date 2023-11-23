4fae76f0ee9abe6e690e3c34c8174ba3d80c9844
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-23 17:06:03 +0100

    [UPDATE] Ottimizzazione del codice

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {                   // Se i è divisibile per 3
-        console.log("Fizz");            // Stampa Fizz
-    } else if (i % 5 == 0) {            // Se i è divisibile per 5
-        console.log("Buzz");            // Stampa Buzz
-    } else if (i % 15 == 0) {           // Se i è divisibile per 15
-        console.log("FizzBuzz");        // Stampa FizzBuzz
-    } else {                            // In tutti gli altri casi
-        console.log(i)                  // Stampa il valore di I
-    }
+    let output = "";
+    let list = document.getElementById('list')
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+    console.log(output || i);
+    list.innerHTML += `<li class="list-group-item">${output || i}</li>`;
 }
